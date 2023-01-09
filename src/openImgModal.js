385a46f6f69e72013cacdadfcfc0c4e322c8241a
Andre Espinoza <ae95caba@gmail.com>
2023-01-09 06:09:05 -0300

    improve function naming on openImgModal.js

--- src/openImgModal.js
+++ src/openImgModal.js
@@ -2,25 +2,25 @@ function openImgModal() {
   const img = document.getElementById("me");
   const imgModal = document.getElementById("me-modal");
   const closeButton = document.querySelector("#me-modal button");
-  function funfun(e) {
+  function closeOnClickOut(e) {
     if (e.target.closest(".me-modal-container") == null) {
       imgModal.style.visibility = "hidden";
-      document.removeEventListener("click", funfun);
+      document.removeEventListener("click", closeOnClickOut);
     }
   }
-  function closeOnClickOut() {
-    document.addEventListener("click", funfun);
-  }
+
   //alert("runing openImg");
   img.addEventListener("click", () => {
     //alert("img clicked");
     imgModal.style.visibility = "visible ";
-    setTimeout(closeOnClickOut, 10);
+    setTimeout(() => {
+      document.addEventListener("click", closeOnClickOut);
+    }, 10);
   });
 
   closeButton.addEventListener("click", () => {
     imgModal.style.visibility = "hidden";
-    document.removeEventListener("click", funfun);
+    document.removeEventListener("click", closeOnClickOut);
   });
 }
 export default openImgModal;
